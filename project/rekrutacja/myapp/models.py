7ae79fc04b4925a9ed552203f1c32bf89e7abe4c
Sophie-1227 <demeter.1227.wroclaw@gmail.com>
2023-12-04 19:16:56 +0100

    migration to db

--- project/rekrutacja/myapp/models.py
+++ project/rekrutacja/myapp/models.py
@@ -1,6 +1,116 @@
 from django.db import models
 
 # Create your models here.
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+    first_name = models.CharField(max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class DjangoAdminLog(models.Model):
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    action_time = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
 class Personal_data(models.Model):
     TAK="Tak"
     NIE="Nie"
@@ -16,17 +126,20 @@ class Personal_data(models.Model):
         (MEZCZYZNA, "Mężczyzna"),
         (INNA, "Inna")
     ]
+    # ID = models.ForeignKey(auth.user, related_name=id, unique=True, notnull=True)
+    pesel = models.IntegerField(max_length=11)
     first_name = models.CharField(max_length=100)
     second_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
-    phone = models.IntegerField(length=9) #TODO add country code and template
+    phone = models.IntegerField(max_length=9) #TODO add country code and template
     date_of_birth = models.DateField
     father_name = models.CharField(max_length=100)
     is_polish = models.CharField(max_length=3, choices=tak_nie, default=TAK)
     sex = models.CharField(max_length=9, choices=sex_choices)
 
 class Adress(models.Model):
+    # ID = models.ForeignKey(auth.user, related_name=id, unique=True, notnull=True)
     country = models.CharField(max_length=100)
     postal_code = models.CharField(max_length=6) #TODO: create template for anwsering with _ _ - _ _ _
     city = models.CharField(max_length=100)
@@ -35,6 +148,7 @@ class Adress(models.Model):
     apartment_number = models.CharField(max_length=4)
 
 class High_school(models.Model):
+    # ID = models.ForeignKey(auth.user, related_name=id, unique=True, notnull=True)
     LICEUM_O = "Liceum Ogólnokształcące"
     LICEUM_P = "Liceum Profilowane"
     TECHNIKUM = "Technikum"
@@ -51,8 +165,9 @@ class High_school(models.Model):
     school_city = models.CharField(max_length=100)
 
 class Documents_matura(models.Model):
+    # ID = models.ForeignKey(auth.user, related_name=id, unique=True, notnull=True)
     exam_type = models.CharField(max_length=100) # TODO: add choices
-    exam_year = models.IntegerField(length=4)
+    exam_year = models.IntegerField(max_length=4)
     exam_number = models.CharField(max_length=13)
     exam_issuer = models.CharField(max_length=100)
     exam_date = models.DateField
@@ -60,24 +175,27 @@ class Documents_matura(models.Model):
     exam_country = models.CharField(max_length=100)
 
 class Documents_achivment(models.Model):
+    # ID = models.ForeignKey(auth.user, related_name=id, unique=True, notnull=True)
     achivment_type= models.CharField(max_length=100) # TODO: add choices
     achivment_result= models.CharField(max_length=50) # TODO: add choices
-    achivment_year= models.IntegerField(length=4)
+    achivment_year= models.IntegerField(max_length=4)
     achivment_issuer = models.CharField(max_length=100)
     achivment_city = models.CharField(max_length=100)
     achivment_country = models.CharField(max_length=100)
 
 class Documents_dyploma(models.Model):
+    # ID = models.ForeignKey(auth.user, related_name=id, unique=True, notnull=True)
     dyploma_type= models.CharField(max_length=100) # TODO: add choices
     dyploma_result= models.CharField(max_length=3) # TODO: add choices
-    dyploma_avg= models.DecimalField(max_length=5)
-    dyploma_year= models.IntegerField(length=4)
+    dyploma_avg= models.DecimalField(decimal_places=3, max_digits=5)
+    dyploma_year= models.IntegerField(max_length=4)
     dyploma_issuer = models.CharField(max_length=100)
     dyploma_city = models.CharField(max_length=100)
     dyploma_country = models.CharField(max_length=100)
 
 
 class Matura_results(models.Model):
+    # ID = models.ForeignKey(auth.user, related_name=id, unique=True, notnull=True)
     polski_p = models.IntegerField(max_length=3)
     polski_r = models.IntegerField(max_length=3)
     matematyka_p = models.IntegerField(max_length=3)
@@ -96,6 +214,7 @@ class Matura_results(models.Model):
     informatyka_r = models.IntegerField(max_length=3)
 
 # class Preferences(models.Model): #redundancja -> widok?
+#     ID = models.ForeignKey(auth.user, related_name=id, unique=True, notnull=True)
 #     preference1 = models.CharField(max_length=3)
 #     preference2 = models.CharField(max_length=3)
 #     preference3 = models.CharField(max_length=3)
@@ -104,10 +223,11 @@ class Matura_results(models.Model):
 #     preference6 = models.CharField(max_length=3)
 
 class Applications(models.Model):
-    preference = models.IntegerField(max_length=1, max=6)
-    faculty = models.CharField(length=3) # TODO: add choices / automation based on major
-    major = models.CharField(length=3)
-    tour = models.IntegerField(length=1, max=6)
+    # ID = models.ForeignKey(auth.user, related_name=id, unique=True, notnull=True)
+    preference = models.IntegerField(max_length=1)
+    faculty = models.CharField(max_length=3) # TODO: add choices / automation based on major
+    major = models.CharField(max_length=3)
+    tour = models.IntegerField(max_length=1)
     is_active = models.BooleanField
     is_condition = models.BooleanField
     score = models.IntegerField(max_length=3)
