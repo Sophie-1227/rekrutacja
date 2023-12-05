eb750f29bc7413f8861d9d757db4466239e8f54e
Sophie-1227 <demeter.1227.wroclaw@gmail.com>
2023-12-05 01:37:05 +0100

    added style for personal data form

--- project/rekrutacja/myapp/forms.py
+++ project/rekrutacja/myapp/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from myapp.models import Personal_data, Adress, High_school, Documents_matura, Documents_achivment, Documents_dyploma, Matura_results, Applications
-
+from tempus_dominus.widgets import DatePicker
 
 class SignupForm(UserCreationForm):
     class Meta:
@@ -16,14 +16,28 @@ class LoginForm(forms.Form):
 class PersonalDataForm(forms.ModelForm):
     class Meta:
         model = Personal_data
-        fields = ['pesel', 'first_name', 'last_name', 'email', 'phone', 'date_of_birth', 'father_name', 'is_polish', 'sex']
-    # pesel = forms.IntegerField()
-    # first_name = forms.CharField()
-    # second_name = forms.CharField()
-    # last_name = forms.CharField()
-    # email = forms.CharField(widget=forms.EmailInput)
-    # phone = forms.CharField()
-    # date_of_birth = forms.DateField(widget=forms.Date)
-    # father_name = forms.CharField()
-    # is_polish = forms.BooleanField(widget=forms.BooleanInput)
-    # sex = 
+        fields = ['pesel', 'first_name', 'second_name', 'last_name', 'email', 'phone', 'date_of_birth', 'father_name', 'is_polish', 'sex']
+        labels = {
+            'pesel': 'PESEL',
+            'first_name': 'Imię',
+            'second_name': 'Drugie imię',
+            'last_name': 'Nazwisko',
+            'email': 'Adres e-mail',
+            'phone': 'Numer telefonu',
+            'date_of_birth': 'Data urodzenia',
+            'father_name': 'Imię ojca',
+            'is_polish': 'Czy posiadasz polskie obywatelstwo',
+            'sex': 'Płeć',
+        }
+        widgets = {
+            'pesel': forms.TextInput(attrs={'class': 'input'}),
+            'first_name': forms.TextInput(attrs={'class': 'input'}),
+            'second_name': forms.TextInput(attrs={'class': 'input'}),
+            'last_name': forms.TextInput(attrs={'class': 'input'}),
+            'email': forms.EmailInput(attrs={'class': 'input'}),
+            'phone': forms.TextInput(attrs={'class': 'input'}),
+            'father_name': forms.TextInput(attrs={'class': 'input'}),
+            'date_of_birth': forms.DateInput(attrs={'class': 'input'}),
+            'is_polish': forms.CheckboxInput(attrs={'class': 'input'}),
+            'sex': forms.Select(attrs={'class': 'input'}),
+        }
